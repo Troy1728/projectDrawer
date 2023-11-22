d11ee38d3df2ec79fe979353575ae533984cf919
Troy1728 <schepens.jarno@gmail.com>
2023-11-22 21:58:30 +0100

    verwijderen code in commentaar

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,45 +1,8 @@
-/*
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getFirestore} from 'firebase/firestore';
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBaF7sKRmwxWABJBEZDVE4AGVHq67wDRGo",
-  authDomain: "projectdrawer-fac4e.firebaseapp.com",
-  projectId: "projectdrawer-fac4e",
-  storageBucket: "projectdrawer-fac4e.appspot.com",
-  messagingSenderId: "53795767287",
-  appId: "1:53795767287:web:2ac09cce3d5cf9dba2a4f2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const FIREBASE_AUTH = getAuth(app);
-const FIREBASE_DB = getFirestore(app);
-
-export { app, FIREBASE_AUTH, FIREBASE_DB }
-*/
-
-
-/*
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { ReactNativeAsyncStorage } from "@react-native-async-storage/async-storage";
-import { app } from "./FirebaseConfig.js";
-
-const ASYNC_AUTH = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
-
-export { ASYNC_AUTH };
-*/
-
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore} from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 const firebaseConfig = {
   apiKey: "AIzaSyBaF7sKRmwxWABJBEZDVE4AGVHq67wDRGo",
   authDomain: "projectdrawer-fac4e.firebaseapp.com",
@@ -49,7 +12,6 @@ const firebaseConfig = {
   appId: "1:53795767287:web:2ac09cce3d5cf9dba2a4f2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const FIREBASE_AUTH = initializeAuth(app, {
     persistence: getReactNativePersistence(AsyncStorage),

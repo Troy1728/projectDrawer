b80aba7452f4004c40ed74a973bfb4122e93ca2b
Jarno Schepens <jarno.schepens@student.odisee.be>
2024-01-06 14:47:59 +0100

    show button to stock if user role is admin or dev

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,44 @@
 import { View, Text, TouchableOpacity, ActivityIndicator, KeyboardAvoidingView, StyleSheet, Dimensions } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import stylesFile from '../styles';
 import { StatusBar } from 'expo-status-bar';
 import CustomButton from '../atoms/CustomButton';
+import { db, FIREBASE_AUTH } from '../components/Firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
 export default function HomeScreen({ navigation }) {
+  const [role, setRole] = useState(null);
+
+  const getLoggedInUser = async () => {
+    const data = await getDocs(
+      query(
+        collection(db, "users"),
+        where("id", "==", FIREBASE_AUTH.currentUser.uid)
+      )
+    );
+    const role = data.docs[0].data().role;
+    setRole(role);
+  };
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, []);
+
+
   return (
     <View style={[stylesFile.container]}>
       <View style={[styles.titleContainer]}>
         <Text style={[stylesFile.title]}>Donator</Text>
       </View>
-      <View style={{ justifyContent: "space-evenly" }}>
-        <CustomButton
-          title="Magazijn"
-          buttonDesign="fullButton"
-          onPress={() => navigation.navigate("StockScreen")}
-        />
-      </View>
+      {role === "admin" || role === "dev" ? (
+        <View style={{ justifyContent: "space-evenly" }}>
+          <CustomButton
+            title="magazijn"
+            buttonDesign="fullButton"
+            onPress={() => navigation.navigate("StockScreen")}
+          />
+        </View>
+      ) : null}
       <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
         <CustomButton
           title="Profiel"
